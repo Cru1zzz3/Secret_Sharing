2446e1149ae069d620f68b38afa5aff193473701
Stanislav Udartsev <udartzev_sv@mail.ru>
2018-12-02 17:00:30 +0300

    Final implementation of calculating lagrange polynom

--- Source.cpp
+++ Source.cpp
@@ -36,21 +36,25 @@ vector<int> gen_shadows(int k, int& M, int p, int n, vector <int> shadows){
 	mt19937 generator(random_device2());
 	uniform_int_distribution<> distribution(1, LIMIT);
 	vector <int> arrX;
-	arrX.push_back(0);
-	arrX.push_back(8);
-	arrX.push_back(7);
 
-
-	/*
 	for (int i = 0; i < k - 1; i++){
 	arrX.push_back(distribution(generator));
 	}
+
+	/*
+	arrX.push_back(0);
+	arrX.push_back(8);
+	arrX.push_back(7);
 	*/
+
+	
+	
+	
 
 	for(int x = 1; x < (n + 1); x++){
 		shadows.push_back(0);
 		for(int i = k - 1; i > 0; i--){
-			shadows[x] += arrX[i] * pow(x,i);
+			shadows[x] += arrX[i-1] * pow(x,i-1);
 		}
 		shadows[x] += M;
 		shadows[x] = shadows[x] % p;
@@ -63,7 +67,17 @@ vector<int> gen_shadows(int k, int& M, int p, int n, vector <int> shadows){
 	return shadows;
 }
 
-vector <int> multiply_polynoms(vector<int> polynom1,vector<int> polynom2){
+vector <int> add_polynoms(vector<int> polynom1,vector<int> polynom2,int N){
+	vector<int> sumPolynom;
+	for (int i = 0; i < polynom1.size(); i++){
+		int sum = (polynom1[i] + polynom2[i]) % N;
+		sumPolynom.push_back(sum);
+	}
+	return sumPolynom;
+}
+
+
+vector <int> multiply_polynoms(vector<int> polynom1,vector<int> polynom2,int N){
 	vector<int> multipliedPolynom;
 	//vector<int> polynom1 = basisPolynoms[0];
 	//vector<int> polynom2 = basisPolynoms[1];
@@ -82,22 +96,40 @@ vector <int> multiply_polynoms(vector<int> polynom1,vector<int> polynom2){
 			multipliedPolynom[i] = 13 + multipliedPolynom[i];
 		else
 			multipliedPolynom[i] = multipliedPolynom[i] % 13;
-		}
+	}
 
 	return multipliedPolynom;
 }
 
+int gcd (int a, int b, int & x, int & y) {
+	if (a == 0) {
+		x = 0; y = 1;
+		return b;
+	}
+	int x1, y1;
+	int d = gcd (b%a, a, x1, y1);
+	x = y1 - (b / a) * x1;
+	y = x1;
+	return d;
+}
 
-vector <int> calculating_basis_polynom(vector<idShadowStruct> idShadowVector, int k){
+int reverse_element(int a, int N){
+	int x, y;
+	int g = gcd (a, N, x, y);
+	if (g != 1)
+		cout << "no solution";
+	else {
+		x = (x % N + N) % N;
+		return x;
+	}
+}
+vector <int> calculating_lagrange_polynom(vector<idShadowStruct> idShadowVector, int k,vector<int> lagrangePolynom, int N){
 
-	vector<int> basisPolynom; // vector describing polynom
-	vector<vector<int>> basisPolynoms;
-	vector<int> multipliedPolynom;
-	//basisPolynoms.push_back(basisPolynom);
-
-	//for (int i = 1; i < k; i++){}
-	int denominator = 1;
 	for(int i = 0; i < k; i++){
+		vector<int> basisPolynom; // vector describing polynom
+		vector<vector<int>> basisPolynoms;
+		vector<int> multipliedPolynom;
+		int denominator = 1;		
 		for (int j = 0; j < k; j++){
 
 			if (i != j){
@@ -107,87 +139,75 @@ vector <int> calculating_basis_polynom(vector<idShadowStruct> idShadowVector, in
 				basisPolynoms.push_back(basisPolynom);
 
 				denominator *= idShadowVector[i].id - idShadowVector[j].id;
-				
+
 				basisPolynom.clear();
 			}
 		}
-		
-		multipliedPolynom = multiply_polynoms(basisPolynoms[0],basisPolynoms[1]); // (x^2 - 8x + 15)
-		denominator = 9; // 3
+
+		multipliedPolynom = multiply_polynoms(basisPolynoms[0],basisPolynoms[1],N); // (x^2 - 8x + 15)
+
+		// знаменатель в обратный по модулю
+		denominator = reverse_element(denominator,N); // 3
 		vector<int> denominatorVector;
 		denominatorVector.push_back(denominator);
-		// алгоритм евклида к знаменателю
-		// 
-		multipliedPolynom = multiply_polynoms(denominatorVector,multipliedPolynom); 
 
+		//перемножение числителя и знаменателя
+		multipliedPolynom = multiply_polynoms(denominatorVector,multipliedPolynom,N);
+
+		//первое слагаемое полинома лагранжа
+		vector<int> convertToVector;
+		convertToVector.push_back(idShadowVector[i].shadow);
+		vector<int> lagrangeSummand = multiply_polynoms(convertToVector,multipliedPolynom,N);
+
+		while (lagrangePolynom.size() < lagrangeSummand.size())
+			lagrangePolynom.push_back(0);
+		lagrangePolynom = add_polynoms(lagrangeSummand,lagrangePolynom,N);
 	}
-
-	
-
-
-	vector<int> polinomFirst;
-	vector<int> polynomSecond;
-	// (x - xj)/(xi - xj)
-
-	for (int j = 0; ;j++){
-		for (int i = 0; ;i++){
-
-		}
-	}
-
-
-	return basisPolynom;
+	return lagrangePolynom;
 };
 
 
-vector <int> interpolation_lagrange_polynom(vector<idShadowStruct> idShadowVector, int k){
-	vector <int> lagrange_polynom;
-	lagrange_polynom.push_back(-1);
-
-	for (int i = 1; i < k; i++){
-		calculating_basis_polynom(idShadowVector,k);
-		lagrange_polynom[i] = idShadowVector[i].shadow;
-	}
-
-	return lagrange_polynom; 
+int recovery_secret(vector<idShadowStruct> idShadowVector, int k,int N){
+	vector<int> lagrangePolynom;
+	lagrangePolynom = calculating_lagrange_polynom(idShadowVector, k,lagrangePolynom,N);
+	
+	int secret = lagrangePolynom[0];
+	return secret;
 }
 
-
-
-void recovery_secret(){
-
-}
 
 
 
 
 int main(){
+
+
+
+
 	int M;
 	cout << "Input secret: ";
-	//cin >> M;
-	M = 11;
+	cin >> M;
+	//M = 11;
 
-	//int p = gen_p(M);
-	int p = 13;
-	cout << p << endl;
+	int p = gen_p(M);
+	//int p = 13;
+	cout << "Module: " << p << endl;
 
 	int n;
 	cout << "Input amount of people in coalition: ";
-	//cin >> n;
-	n = 5;
+	cin >> n;
+	//n = 5;
 
 	int k;
 	cout << "Input threshold number of people in coalition: ";
-	//cin >> k;
-	k = 3;
+	cin >> k;
+	//k = 3;
 
 	vector <int> shadows;
 	shadows.push_back(-1);
 
-	//shadows = gen_shadows(k, M, p, n, shadows);
-	//shadows.clear();
-	
-	//recovery_secret();
+	shadows = gen_shadows(k, M, p, n, shadows);
+	shadows.clear();
 
 	int id;
 	int sh;
@@ -195,15 +215,23 @@ int main(){
 	idShadowStruct idShadow;
 	vector<idShadowStruct> idShadowVector;
 
-	for (int i = 0; i < 3; i++){
+	int N;
+	cout << "Input module: ";
+	cin	 >> N;
+	//N = 13;
+
+	//int k;
+	cout << "Input k: ";
+	cin >> k;
+	for (int i = 0; i < k; i++){
 		cout << "Input id: ";
 		cin	 >> idShadow.id;
-			if (idShadow.id == 'n')
-				break;
+		if (idShadow.id == 'n')
+			break;
 		cout << "Input shadow of key: ";
 		cin	 >> idShadow.shadow;
-			if (idShadow.shadow == 'n')
-				break;
+		if (idShadow.shadow == 'n')
+			break;
 		cout << endl;
 		idShadowVector.push_back(idShadow);
 		/*
@@ -211,19 +239,19 @@ int main(){
 		cout << "Do you know another id and shadow of key? (y/n)" << endl;
 		cin >> ans;
 		if (ans != 'y' && ans != 'n'){
-			cout << "Incorrect input! Try again" << endl;
-			ans = NULL;
-			goto reenter;
+		cout << "Incorrect input! Try again" << endl;
+		ans = NULL;
+		goto reenter;
 		}
-			
-		if (ans == n)
-			break;
-			*/
-	}
-	
-	
-	interpolation_lagrange_polynom(idShadowVector, k);
 
+		if (ans == n)
+		break;
+		*/
+	}
+
+
+	int secret = recovery_secret(idShadowVector,k , N);
+	cout << "Secret is: " << secret << endl;
 	system("pause");
 	return 0;
 }
